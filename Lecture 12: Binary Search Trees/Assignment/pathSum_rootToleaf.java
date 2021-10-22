35ca0a0b1187b6b0fce414e5544aefc257b3b6e7
Ishan Goyal <ishan.goyal1305@gmail.com>
2021-10-22 11:17:25 +0530

    Path Sum Root to Leaf

--- Lecture 12: Binary Search Trees/Assignment/pathSum_rootToleaf.java
+++ Lecture 12: Binary Search Trees/Assignment/pathSum_rootToleaf.java
@@ -1,43 +1,22 @@
-/*
-	
-	Following is the structure used to represent the Binary Tree Node
-
-	class BinaryTreeNode<T> {
-		T data;
-		BinaryTreeNode<T> left;
-		BinaryTreeNode<T> right;
-
-		public BinaryTreeNode(T data) {
-			this.data = data;
-			this.left = null;
-			this.right = null;
-		}
-	}
-
-*/
-
-public class Solution {
-
-	public static void rootToLeafPathsSumToK(BinaryTreeNode<Integer> root, int k) {
-
-        rootToLeafPathsSumToK(root, k,"");
-
+public class Solution 
+{
+    private static void rootToLeafPathsSumToK(BinaryTreeNode<Integer> root, int k, String path, int currSum) 
+    {
+        if(root == null) 
+        {
+            return;
+        } if (root.left == null && root.right == null) 
+        {
+            currSum += root.data; 
+            if (currSum == k) { 
+                System.out.println((path + root.data + " ")); 
+            }
+            return;
+        } rootToLeafPathsSumToK(root.left, k, (path + root.data + " "), (currSum + root.data)); 
+        rootToLeafPathsSumToK(root.right, k, (path + root.data + " "), (currSum + root.data));
     }
-    public static void rootToLeafPathsSumToK(BinaryTreeNode<Integer> root, int k,String output) {
-
-    	 if(root==null){
-             return ;
-         }
-    	
-        if(k==root.data) {
-            System.out.println(output+root.data);
-            return ;
-        }
-       
-        rootToLeafPathsSumToK(root.left, k - root.data, output + root.data+" " );
-        rootToLeafPathsSumToK(root.right, k - root.data, output + root.data+" " );
-
-
+    public static void rootToLeafPathsSumToK(BinaryTreeNode<Integer> root, int k) 
+    {
+        rootToLeafPathsSumToK(root, k, "", 0);
     }
-
 }
